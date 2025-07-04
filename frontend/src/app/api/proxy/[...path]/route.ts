93e6a2d979e37d42af0760c21bdada7d498cdf61
GoiToLaTri <dthanhtri1111@gmail.com>
2025-07-04 21:40:32 +0700

    remove export handle proxy

--- frontend/src/app/api/proxy/[...path]/route.ts
+++ frontend/src/app/api/proxy/[...path]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const BACKEND_URL = envConfig.BACKEND_URL;
 
-export async function handleProxy(
+async function handleProxy(
   req: NextRequest,
   context: Promise<{ params: { path: string[] } }>
 ) {
