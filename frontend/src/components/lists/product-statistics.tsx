c10090b319ca252268b2519570cbc748860501c1
GoiToLaTri <dthanhtri1111@gmail.com>
2025-07-21 13:37:43 +0700

    fix ui statistics

--- frontend/src/components/lists/product-statistics.tsx
+++ frontend/src/components/lists/product-statistics.tsx
@@ -4,35 +4,40 @@ import { useGetProductStatistics } from "@/hooks/useGetProductStatistics";
 import { Descriptions, Rate, Skeleton } from "antd";
 
 export default function ProductStatistics({ id }: { id: string }) {
-  const { data, isLoading, isFetching } = useGetProductStatistics(id);
+  const { data, isLoading } = useGetProductStatistics(id);
+
   return (
-    <Descriptions
-      column={4}
-      styles={{
-        title: {
-          fontSize: "20px",
-          lineHeight: "1.75rem",
-          fontWeight: 600,
-          color: "#fff",
-        },
-        label: { fontSize: "16px" },
-        content: { fontSize: "16px" },
-      }}
-    >
-      {(isLoading || isFetching) && <Skeleton />}
-      <Descriptions.Item label="Rate">
-        <Rate allowHalf disabled defaultValue={data?.averageRating} />
-      </Descriptions.Item>
-      <Descriptions.Item label="Review">
-        {data?.ratingCount || 0}
-      </Descriptions.Item>
-      <Descriptions.Item label="View">
-        {data?.interactions.filter((item) => item.action === "VIEW")[0].count ||
-          0}
-      </Descriptions.Item>
-      <Descriptions.Item label="Saled">
-        {data?.totalSold || 0}
-      </Descriptions.Item>
-    </Descriptions>
+    <div>
+      {isLoading && <Skeleton loading />}
+      {data && (
+        <Descriptions
+          column={4}
+          styles={{
+            title: {
+              fontSize: "20px",
+              lineHeight: "1.75rem",
+              fontWeight: 600,
+              color: "#fff",
+            },
+            label: { fontSize: "16px" },
+            content: { fontSize: "16px" },
+          }}
+        >
+          <Descriptions.Item label="Rate">
+            <Rate allowHalf disabled defaultValue={data.averageRating} />
+          </Descriptions.Item>
+          <Descriptions.Item label="Review">
+            {data?.ratingCount || 0}
+          </Descriptions.Item>
+          <Descriptions.Item label="View">
+            {data.interactions.filter((item) => item.action === "VIEW")[0]
+              .count || 0}
+          </Descriptions.Item>
+          <Descriptions.Item label="Saled">
+            {data.totalSold || 0}
+          </Descriptions.Item>
+        </Descriptions>
+      )}
+    </div>
   );
 }
